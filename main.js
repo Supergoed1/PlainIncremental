1096e5e39a091c4f0276c662c0047576879c8481
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-04-27 20:24:57 +0200

    Added basic content

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 var game = {
-    money: new Decimal(0),
-    moneyPerClick: new Decimal(1)
+    money: 0,
+    moneyPerClick: 1,
+    prestigeCoins: 0,
+    bonusPerPresCoin: 1,
+    clickUpgradeCost: 10,
+    clickAmountPerUpgrade: 1
 };
+
+var updateguiint = setInterval("updateGui()", 20);
 
 function init() {
     console.log("hi");
 }
 
-function onClick() {
-    game.money = game.money.plus(moneyPerClick);
-    console.log(game.money.toString());
+function updateGui() {
+    document.getElementById("money").innerHTML = format(game.money);
 }
 
+function onClick() {
+    game.money += game.moneyPerClick;
+}
 
+function format(num) {
+    return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
 
+function buyUpgrade(upgrade) {
+    if(upgrade == "click") {   
+        if(game.money >= game.clickUpgradeCost) {
+            game.moneyPerClick += game.clickAmountPerUpgrade;
+            game.money -= game.clickUpgradeCost;
+            game.clickUpgradeCost *= 2;
+        }
+    }
+}
 
 init();
